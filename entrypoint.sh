0a4496fab7cd3e4ba4ee18aa9450421be1195214
esme-jo <esme.jo@kakaoent.com>
2023-04-11 18:51:37 +0900

    chore: extension 받을 수 있도록 추가

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ -z "$GITHUB_TOKEN" ]]; then
+if [ -z "$GITHUB_TOKEN" ]; then
   echo "Set the GITHUB_TOKEN environment variable."
   exit 1
 fi
--- fix.py
+++ fix.py
@@ -3,11 +3,11 @@ import json
 import html
 import re
 from typing import Union
+from typing import Optional
 
 import requests
 from github import Github
 from whatthepatch import parse_patch
-import github.GithubObject
 
 def fix(original: str) -> str:
     response = requests.get(
@@ -17,13 +17,14 @@ def fix(original: str) -> str:
     return html.unescape(response.json()['message']['result']['notag_html'])
 
 
-def comment_fix_suggestion(gh_token: str, repo_name: Union[str, int], pr_number: int, target: str) -> None:
+def comment_fix_suggestion(gh_token: str, repo_name: Union[str, int], pr_number: int, target: str, extensions: Optional[str]) -> None:
     g = Github(gh_token)
     pr = g.get_repo(repo_name).get_pull(pr_number)
     commits = pr.get_commits()
     comments = []
+    extensions = extensions.split(",") if extensions else []
     for file in pr.get_files():
-        if (target and not re.match(target, file.filename)) or file.filename.split(".")[-1] != "md":
+        if (target and not re.match(target, file.filename)) or (len(extensions) > 0 and file.filename.split(".")[-1] not in extensions):
             continue
         for diff in parse_patch(file.patch):
             for change in diff.changes:
@@ -48,5 +49,6 @@ if 'GITHUB_EVENT_PATH' in os.environ:
             gh_token=os.environ.get('GITHUB_TOKEN'),
             repo_name=os.environ.get('GITHUB_REPOSITORY'),
             pr_number=json_data['issue']['number'],
-            target=os.environ.get('INPUT_TARGET')
+            target=os.environ.get('INPUT_TARGET'),
+            extensions=os.environ.get('INPUT_EXTENSIONS')
         )
